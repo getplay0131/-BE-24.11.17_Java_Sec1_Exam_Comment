c2400e220aafcab6d485e01d0057e504154dcf9d
seong_hyun <getplay0131>
2024-11-11 17:31:26 +0900

    24.11.11 update

---
+++ src/Exam_241111/Ex5.java
@@ -0,0 +1,27 @@
+// 3. 조건문 연습
+// 문제 3-1: 계절 판별기
+// 월(1~12)을 입력받아 해당 계절을 출력하세요.
+//
+// 봄: 3~5월
+// 여름: 6~8월
+// 가을: 9~11월
+// 겨울: 12~2월
+
+package Exam_241111;
+
+import java.util.Scanner;
+
+public class Ex5 {
+  public static void main(String[] args) {
+
+      Scanner scan = new Scanner(System.in);
+
+    System.out.print("확인할 월을 입력해 주세요 : ");
+      int season = scan.nextInt();
+
+      if (season >= 3 && season <= 5) {
+
+      }
+
+  }
+}
